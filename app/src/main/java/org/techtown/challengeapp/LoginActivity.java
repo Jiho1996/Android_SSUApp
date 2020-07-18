ea93035015833c1b7e04e8c8f5ccd67c3ccdddf4
jiho1996 <jhp9270@naver.com>
2020-07-18 19:37:49 +0900

    insert RegisterActivity

--- app/src/main/java/org/techtown/challengeapp/LoginActivity.java
+++ app/src/main/java/org/techtown/challengeapp/LoginActivity.java
@@ -2,7 +2,13 @@ package org.techtown.challengeapp;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.TextView;
+
+import javax.security.auth.login.LoginException;
+
 
 public class LoginActivity extends AppCompatActivity {
 
@@ -10,5 +16,16 @@ public class LoginActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login);
-    }
+
+    TextView registerButton = (TextView) findViewById(R.id.registerButton);
+    registerButton.setOnClickListener(new View.OnClickListener(){
+
+
+        @Override
+        public void onClick(View view){
+            Intent registerIntent = new Intent(LoginActivity.this,RegisterActivity.class);
+            LoginActivity.this.startActivity(registerIntent);
+        }
+    });
+}
 }
